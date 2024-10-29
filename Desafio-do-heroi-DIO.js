f3ce2ca5cd56238ba0ca723c3a1eb8c4bf35b490
Richard-gtb <richardbarbosadesouza@gmail.com>
2024-10-29 13:14:02 -0300

    corrção do código

--- Desafio-do-heroi-DIO.js
+++ Desafio-do-heroi-DIO.js
@@ -17,32 +17,24 @@
 //Se XP
 //for maior ou igual a 10.001 = Radiante
 
-var nome = "Richard";
-var nivel = "7000";
 
-switch (nivel) {
-    case (nivel < 1000):
-        console.log("O Herói de nome " + nome + " está no nível de Ferro");
-        break;
-    case 1001 <= nivel <= 2000:
-        console.log("O Herói de nome " + nome + " está no nível de Bronze");
-        break;
-    case 2001 <= nivel <= 5000:
-        console.log("O Herói de nome " + nome + " está no nível de Prata");
-        break;
-    case ("5001" <= nivel <= "7000"):
-        console.log("O Herói de nome " + nome + " está no nível de Ouro");
-        break;
-    case 7001 <= nivel <= 8000:
-        console.log("O Herói de nome " + nome + " está no nível de Platina");
-        break;
-    case 8001 <= nivel <= 9000:
-        console.log("O Herói de nome " + nome + " está no nível de Ascendente");
-        break;
-    case 9001 <= nivel <= 10000:
-        console.log("O Herói de nome " + nome + " está no nível de Imortal");
-        break;
-    case nivel >= 10001:
-        console.log("O Herói de nome " + nome + " está no nível de Radiante");
-        break;
+var nome = "Richard";
+var nivel = 1000;
+
+if (nivel < 1000) {
+    console.log("O Herói de nome " + nome + " está no nível de Ferro");
+} else if (nivel >= 1001 && nivel <= 2000) {
+    console.log("O Herói de nome " + nome + " está no nível de Bronze");
+} else if (nivel >= 2001 && nivel <= 5000) {
+    console.log("O Herói de nome " + nome + " está no nível de Prata");
+} else if (nivel >= 5001 && nivel <= 7000) {
+    console.log("O Herói de nome " + nome + " está no nível de Ouro");
+} else if (nivel >= 7001 && nivel <= 8000) {
+    console.log("O Herói de nome " + nome + " está no nível de Platina");
+} else if (nivel >= 8001 && nivel <= 9000) {
+    console.log("O Herói de nome " + nome + " está no nível de Ascendente");
+} else if (nivel >= 9001 && nivel <= 10000) {
+    console.log("O Herói de nome " + nome + " está no nível de Imortal");
+} else if (nivel >= 10001) {
+    console.log("O Herói de nome " + nome + " está no nível de Radiante");
 }
